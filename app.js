b41772cf96f87dec92081fc7b5bdb24fdc39a592
andraroston <andraroston@c162064.wlan.net.ed.ac.uk>
2014-08-15 15:14:11 +0100

    Much Progress
    All functionality in place except changing the radio buttons - still
    working on that.

--- app.js
+++ app.js
@@ -1,11 +1,71 @@
 $(document).ready(function() {
 
-
-
-//to loop through the questions?
-//for (var i = 0; i < allQuestions.length; i++)
+var	allQuestions = [
+	        {
+	        number: 1,
+			question: "What type of animal is Aslan?",
+			possibleAnswers: ["Bear", "Kangaroo", "Lion", "Crab"],
+			correctAnswer: "Lion"
+		},
+		    {
+	        number: 2,
+			question: "What is the name of the youngest Pevensie?",
+			possibleAnswers: ["Lucy", "Lily", "Linda", "Laura"],
+			correctAnswer: "Lucy"
+		},
+		    {
+			number: 3,
+	        question: "How do the Pevensie children get to Narnia the first time?",
+			possibleAnswers: ["Car", "Train", "Raincoat", "Wardrobe"],
+			correctAnswer: "Wardrobe"
+		},
+		    {
+	        number: 4,
+			question: "In what country is the actual town Narina is named after?",
+			possibleAnswers: ["France", "Italy", "USA", "Fiji"],
+			correctAnswer: "Italy"
+		},
+		    {
+	        number: 5,
+			question: "What year was The Lion, The Witch, and the Wardrobe released?",
+			possibleAnswers: ["1956", "1951", "1950", "1954"],
+			correctAnswer: "1950"
+		},
+		    {
+	        number: 6,
+			question: "What kind of creature is Mr Tumnus?",
+			possibleAnswers: ["Centaur", "Faun", "Hipporgriff", "Merman"],
+			correctAnswer: "Faun"
+		},
+		    {
+	        number: 7,
+			question: "What year was the first Narnia film released?",
+			possibleAnswers: ["2001", "2005", "2007", "2003"],
+			correctAnswer: "2005"
+		},
+		    {
+	        number: 8,
+			question: "What sweet treat does the White Witch offer Edmund?",
+			possibleAnswers: ["Chocolate", "Cupcakes", "Turkish Delight", "Banoffee Pie"],
+			correctAnswer: "Turkish Delight"
+		},
+		    {
+	        number: 9,
+			question: "Which is the last book in the series?",
+			possibleAnswers: ["The Horse and his Boy", "The Last Battle", "The Voyage of the Dawn Treader", "The Lion, the Witch, and the Wardrobe"],
+			correctAnswer: "The Last Battle"
+		},
+		    {
+	        number: 10,
+			question: "Which university did C.S. Lewis attend?",
+			possibleAnswers: ["Oxford", "Cambridge", "Edinburgh", "St. Andrews"],
+			correctAnswer: "Oxford"
+		}
+	];
 
 var userScore = 0;
+var currentTurn = 0;
+
 
 //to add to the counter at the bottom right
 var counter = 0;
@@ -27,23 +87,22 @@ var gamePlay = function() {
 	}
 
 //	getting the value of the user choice
+
 	var userChoice = $('input[name=choice]:checked').val()
 		console.log(userChoice);
 
-//  to add one oint to user score if answer is correct
-//	var userScoring = function() {
-//			if(userChoice === this.correctAnswer) {
-//			userScore += 1
-//		};
-//	console.log(userScore);
- 	
-//}	
+//  to add one point to user score if answer is correct
+	var userScoring = function() {
+			if(userChoice === allQuestions[currentTurn].correctAnswer) {
+			userScore += 1
+		};
+	console.log(userScore);
+ 	currentTurn += 1;	
+	};	
 
-}
+	userScoring();
 
-// how to load next question or final score
-
-
+};
 
 
 //what happens when user clicks submit button
@@ -51,17 +110,29 @@ $("#submitButton").on("click", function() {
 	event.preventDefault();
 	console.log("Submit Button Clicked");
 	gamePlay();
+
+// how to load next question and or final score
+	if (currentTurn < 10) {
+		$("#question").replaceWith("<h2 id = 'question'>" + allQuestions[currentTurn].question + "</h2>");
+		//set radio buttons to next turn
+	} else {
 	$("#possibleAnswers").find("ul").hide();
+	$('#finalScore').replaceWith("<span id='finalScore'>" + userScore + "</span>")
 	$("#possibleAnswers").find("h3").show();
 	$("#question").replaceWith("<h2 id = 'question'>" + "All done! Your score is..." + "</h2>");
+	}
+
 });
 
 
 //what happens when user clicks new game button
 $("#newGame").on("click", function(){
 	console.log("New Game Button Clicked")
-	var userScore = 0;
-	var counter = 0;
+	currentTurn = 0;
+	userScore = 0;
+	counter = 0;
 	$('#count').replaceWith("<span id='count'>" + counter + "</span>")
+	$("#question").replaceWith("<h2 id = 'question'>" + allQuestions[currentTurn].question + "</h2>")
+	//reset radio buttons back to turn 0
 	});
 })
